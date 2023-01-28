ab845e23532d5caf9298277f41759ce7a72cb6c6
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-01-27 21:13:24 -0300

    prettier working

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: [require.resolve('prettier-plugin-astro')],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+    "useTabs": true,
+    "singleQuote": false,
+    "jsxSingleQuote": false,
+    "bracketSameLine": true,
+    "arrowParens": "always",
+    "endOfLine": "auto",
+    "singleAttributePerLine": true // Enforce single attribute per line in HTML, Vue and JSX.
+  };
