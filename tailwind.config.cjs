2634580ffa20a9f919350ce4836ed5a66200c8c8
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-03-23 23:02:55 -0300

    added effects and minor details

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			serif: ['Inter', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
-}
+};
