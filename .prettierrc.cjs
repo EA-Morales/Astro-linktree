79129dd10dc150d6b4971e606a76beded2f6822f
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-03-27 23:05:00 -0300

    Eslint and prettier modified

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,18 +1,11 @@
 module.exports = {
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
       },
-    ],
-    "useTabs": true,
-    "singleQuote": true,
-    "jsxSingleQuote": true,
-    "bracketSameLine": true,
-    "arrowParens": "always",
-    "endOfLine": "auto",
-    "singleAttributePerLine": true // Enforce single attribute per line in HTML, Vue and JSX.
-  };
+    },
+  ]
+};
